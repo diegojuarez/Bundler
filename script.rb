50a3061714d529b3d8d40fa8a03165c0a11ab2b6
Diego <diego.utng2015@gmail.com>
2017-01-28 10:14:18 -0600

    cambios

--- script.rb
+++ script.rb
@@ -9,8 +9,8 @@ require './sqlite/sqlite_script'
 require './app/permissions'
 require './pry/propina_script'
 
-admin_personal_info = {'name'=>'Nayeli', 'last_name'=>'Ramirez', 'birth_year'=>1986}
-admin_contact_info = {'email'=>'cnre86@gmail.com', 'mob'=>'4731072629'}
+admin_personal_info = {'name'=>'Diego', 'last_name'=>'Juarez', 'birth_year'=>1995}
+admin_contact_info = {'email'=>'diego.utng2015@gmail.com', 'mob'=>'44535345'}
 admin_role = Role.new('users','contacts','promotions')
 admin = User.new(admin_personal_info, admin_contact_info, admin_role)
 
@@ -22,6 +22,6 @@ ap admin_role.methods - Object.methods
 
 puts tip(80)
 
-save_new_user('Juan Flores ', 'juan.flores01@cfe.gob.mx', 1981)
-users = save_new_user('santi', 'sagoamigo86@hotmail.com.mx', 1986)
+save_new_user('oberto', 'roberto@hotmail.com', 1981)
+users = save_new_user('elcompa', 'elcompa@gmail.com', 1986)
 ap users
