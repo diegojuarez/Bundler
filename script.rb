b5b69f1b0c38329d914c162365957f1f229d366f
Diego <diego.utng2015@gmail.com>
2017-01-28 10:19:43 -0600

    initial commit

--- script.rb
+++ script.rb
@@ -22,6 +22,6 @@ ap admin_role.methods - Object.methods
 
 puts tip(80)
 
-save_new_user('oberto', 'roberto@hotmail.com', 1981)
+save_new_user('oberto', 'roberto@hotmail.com.mx', 1981)
 users = save_new_user('Elcompa', 'elcompa@gmail.com', 1986)
 ap users
